cf97db52300eb74c682672f5cc0bd524b864e5ca
EvanScanlon <56086524+EvanScanlon@users.noreply.github.com>
2019-10-02 18:09:39 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+//C++ time
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+	string input;
+	string open = "``";
+	string close = "''";
+	bool alternate = true;
+	cout << "Enter your input here: " << endl;
+	getline(cin, input);
+
+	for (int i = 0; i < input.length(); i++) {
+		if ((input[i] == '"')) {
+			if (alternate) {
+				input.replace(i, 1, open);
+			}
+			else {
+				input.replace(i,1,close);
+			}
+			alternate = !alternate;
+		}
+	}
+	cout << input << endl;
+	return 0;
+}
